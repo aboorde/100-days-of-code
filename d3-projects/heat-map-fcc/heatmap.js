6037ecf4fa64e03da0bf27460ef605899bb10dbb
aboorde <andrew.boorde@gmail.com>
2017-01-24 22:28:14 -0700

    Finish Heatmap

--- d3-projects/heat-map-fcc/heatmap.js
+++ d3-projects/heat-map-fcc/heatmap.js
@@ -1,11 +1,11 @@
 // set dimensions and margins of map
-var margin = { top: 40, right: 20, bottom: 40, left: 60},
-    width = 1000 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var margin = { top: 50, right: 120, bottom: 160, left: 160},
+    width = 1200 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
 
 
 var colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"];
-
+//var colors = d3.scaleOrdinal(d3.schemeCategory10);
 //set the ranges
 //var x = d3.scaleLinear().range([0, width]),
 //    y = d3.scaleLinear().range([height,0]);
@@ -29,6 +29,35 @@ var svg = d3.select("body").append("svg")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
+function monthName(monthNum) {
+    switch (monthNum) {
+        case 1:
+            return "January";
+        case 2:
+            return "February";
+        case 3:
+            return "March";
+        case 4:
+            return "April";
+        case 5:
+            return "May";
+        case 6:
+            return "June";
+        case 7:
+            return "July";
+        case 8:
+            return "August";
+        case 9:
+            return "September";
+        case 10:
+            return "October";
+        case 11:
+            return "November";
+        case 12:
+            return "December";
+    }
+}
+
 d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json", function(error, data) {
     if(error) throw error;
 
@@ -37,9 +66,10 @@ d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
     heatData.forEach(function(d) {
         d.month = +d.month;
         d.variance = +d.variance;
-        console.log(d.variance*100);
         d.year = +d.year;
         d.date = new Date(d.year,d.month-1);
+        d.monthName = monthName(d.month);
+        //console.log(d.monthName);
     });
 
     var minVariance = d3.min(heatData, function(d) { return d.variance; });
@@ -52,7 +82,7 @@ d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
     y.domain([d3.min(heatData, function(d) { return d.month; }), d3.max(heatData, function(d) { return d.month; })]);
     
     var colorScale = d3.scaleQuantile()
-                .domain([0,8,d3.max(heatData, function(d) { return d.variance * 100; })])
+                .domain([0,9,d3.max(heatData, function(d) { return baseTemperature + d.variance; })])
                 .range(colors);
 
     var cellWidth = Math.floor(width/(d3.max(heatData, function(d) { return d.year; }) - d3.min(heatData, function(d) { return d.year; })));
@@ -68,14 +98,14 @@ d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
         .attr("y", function(d) { return y(d.month) - cellHeight; })
         .attr("width", cellWidth)
         .attr("height", cellHeight)
-        .attr('fill', function(d) { return colorScale(d.variance * 100); })
+        .attr('fill', function(d) { return colorScale(baseTemperature + d.variance); })
         .on("mouseover", function(d) {
             div.transition()
                 .duration(200)
                 .style("opacity", .9);
-            div.html(d.variance + "<br/>" + d.month + ", " + d.year)
-                .style("left", 550 + "px")
-                .style("top", 350 + "px");
+            div.html(d.year + " - " + d.monthName + "<br/>" + (baseTemperature + d.variance) + " &deg;C" + "<br/>" + d.variance + " &deg;C")
+                .style("left", 900 + "px")
+                .style("top", 500 + "px");
             })
             .on("mouseout", function(d) {
             div.transition()
@@ -86,17 +116,70 @@ d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/mas
      // Add the X Axis
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
+        .style("font-size", "18px")
         .call(d3.axisBottom(x)
             .ticks(5)
             .tickFormat(d3.timeFormat("%Y"))
+            //.tickSize(10,0,0)
             );
+
+    // text label for the x axis
+        svg.append("text")             
+            .attr("x", 480 )
+            .attr("y",  440 )
+            .style("text-anchor", "middle")
+            .style("font-weight", "600")
+            .style("font-size", "18px")
+            .text("Year");
+
 
      // Add the Y Axis
     svg.append("g")
         .attr("transform", "translate(0," + (cellHeight / 2 * -1) + ")")
         .attr("class", "no-line")
+        .style("font-size", "12px")
         .call(d3.axisLeft(y)
             .tickPadding(0)
             .tickSize(0,6,0)
+            //.ticks(d3.timeMonths)
+            .tickFormat(function(d) { return monthName(d); })
             );
+
+    // Y Axis text
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -100)
+        .attr("x",0 - (height / 2))
+        .attr("dy", "1em")
+        .style("font-size", "24px")
+        .style("text-anchor", "middle")
+        .text("Month");
+
+    svg.append("text")
+        .attr("x", (width / 2))
+        .attr("y", 10 - (margin.top / 2))
+        .attr("text-anchor", "middle")
+        .style("font-size", "32px")
+        .text("Monthly Global Land-Surface Temperature");
+
+    const legend = svg.selectAll(".legend")
+        .data([0].concat(colorScale.quantiles()), (d) => d);
+
+    const legend_g = legend.enter().append("g")
+        .attr("class", "legend");
+
+    legend_g.append("rect")
+        .attr("x", (d, i) => 50 * i)
+        .attr("y", height + 90)
+        .attr("width", 50)
+        .attr("height", 50)
+        .style("fill", (d, i) => colors[i]);
+
+    legend_g.append("text")
+        .attr("class", "mono")
+        .text((d) => Math.round(d) + "°C")
+        .attr("x", (d, i) => 50 * i + 10)
+        .attr("y", height + 85);
+
+    legend.exit().remove();
 });
